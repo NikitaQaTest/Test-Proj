40b42d805032fa6c591b9bb51823c32013759702
NikitaQaTest <nikita.wolodin@gmail.com>
2023-09-04 17:02:26 +0300

    HW_5.2

--- modules/dev.js
+++ modules/dev.js
@@ -6,6 +6,17 @@ class Dev extends Employee {
     constructor(id, salary) {
       super(id, salary);
       this._jobType = "Dev";
+
+      
+      // только для чтение 
+      Object.defineProperty(this, "id", {
+        enumerable: true,
+        configurable: false,
+        writable: false,
+        value: id,
+       });
+
+
     }
   
     getJobType() {
@@ -13,4 +24,13 @@ class Dev extends Employee {
     }
   }
 
-  export { Dev };
+  export { Dev }
+
+  const newDev = new Dev(1, 3000);
+  
+  newDev.id = 15; // видасть помилку TypeError: Cannot assign to read only property 'id' of object '#<Dev>'
+  delete newDev.id; // видасть помилку TypeError: Cannot delete property 'id' of #<Dev>
+  for (let key in newDev) {
+    console.log(newDev[key]); // виведе почергово 1 та 3000
+  }
+  
